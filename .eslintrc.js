4e4f37ab091a20111c60063ecb73196c4c3c4848
迷渡 <justjavac@gmail.com>
2023-07-11 17:52:20 +0800

    remove some eslint rules (#8)

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,12 +17,7 @@ module.exports = {
     },
   },
   rules: {
-    curly: "error",
-    "@typescript-eslint/ban-types": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-object-literal-type-assertion": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "error",
     "no-console": "error",
